3f19a4b6d3dde94fdca17c4865ec61445a7c079a
NOE-PC\shish <shishimarureo@gmail.com>
2023-10-30 01:24:54 +0900

    chakra ui外してNextUIを導入

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from 'next/image';
 import styles from './page.module.css';
+import { Button } from '@nextui-org/button';
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <div>
+        <Button>Click me</Button>
+      </div>
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,18 +1,11 @@
 'use client';
 
-import { CacheProvider } from '@chakra-ui/next-js';
-import { ChakraProvider } from '@chakra-ui/react';
+import { NextUIProvider } from '@nextui-org/react';
 
-export function Providers({
-  children
-}: {
-  children: React.ReactNode
-  }) {
+export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <CacheProvider>
-      <ChakraProvider>
-        {children}
-      </ChakraProvider>
-    </CacheProvider>
+    <NextUIProvider>
+      {children}
+    </NextUIProvider>
   );
 }
